9ec26f68a19e52689119565437ea5213b9345733
Kurimochi <kurimochi.sec@gmail.com>
2026-01-12 10:47:35 +0900

    feat: Add CID v1

---
+++ src/cid.rs
@@ -0,0 +1,28 @@
+use serde::{Deserialize, Serialize};
+
+pub enum CidVersion {
+    V1,
+}
+
+// Trait common to all versions
+pub trait CidTrait {
+    fn version(&self) -> CidVersion;
+    fn to_vec(&self) -> Vec<u8>;
+}
+
+// CID v1
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Deserialize, Serialize)]
+pub struct CidV1(pub [u8; 32]);
+pub const CIDV1_PREFIX: [u8; 2] = [0x00, 0x01]; // CID v1 Prefix: 0x0001
+
+impl CidTrait for CidV1 {
+    fn version(&self) -> CidVersion {
+        CidVersion::V1
+    }
+    fn to_vec(&self) -> Vec<u8> {
+        let mut result = Vec::with_capacity(self.0.len() + CIDV1_PREFIX.len());
+        result.extend_from_slice(&CIDV1_PREFIX);
+        result.extend_from_slice(&self.0);
+        result
+    }
+}
